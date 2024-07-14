62298c7427277b56f53203e7af5a0e25b83799a1
pham thi thu thuy <phamthithuthuy@MacBook-Air-cua-pham.local>
2024-07-14 15:36:18 +0700

    feat: no project component

---
+++ components/common/NoProjects.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div
+      class="no-projects flex flex-col flex-1 text-center gap-5"
+    >
+      <div class="no-project__icon flex justify-center">
+        <img
+          :src="ProjectIcon"
+          alt="project icon"
+        >
+      </div>
+
+      <p>You don't have any projects yet</p>
+
+      <p>Share, review & approve designs, videos & documents in minutes with HostDesign.</p>
+
+      <div>
+        <button
+          type="button"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Create your first project
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ProjectIcon from '~/assets/images/icon-project.svg'
+
+onMounted(() => {
+})
+</script>
+
+<style lang="scss" scoped>
+.no-projects {
+  padding-top: 10%;
+}
+</style>
--- components/overview/RecentProjects.vue
+++ components/overview/RecentProjects.vue
@@ -9,44 +9,15 @@
     />
   </div>
 
-  <div
-    v-else
-    class="no-projects flex flex-col flex-1 text-center gap-5"
-  >
-    <div class="no-project__icon flex justify-center">
-      <img
-        :src="ProjectIcon"
-        alt="project icon"
-      >
-    </div>
-
-    <p>You don't have any projects yet</p>
-
-    <p>Share, review & approve designs, videos & documents in minutes with HostDesign.</p>
-
-    <div>
-      <button
-        type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Create your first project
-      </button>
-    </div>
-  </div>
+  <NoProjects v-else />
 </template>
 
 <script lang="ts" setup>
 import ProjectOverlay from '~/components/common/ScreenOverlay.vue'
-import ProjectIcon from '~/assets/images/icon-project.svg'
+import NoProjects from '~/components/common/NoProjects.vue'
 
 const data = ref([])
 
 onMounted(() => {
 })
 </script>
-
-<style lang="scss" scoped>
-.no-projects {
-  padding-top: 10%;
-}
-</style>
