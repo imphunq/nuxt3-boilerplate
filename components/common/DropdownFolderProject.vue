e3bc4df7f236d164b5baeeaef850814ba910ce60
Nguyen Quang Phu <nguyen.quang.phu@sun-asterisk.com>
2024-07-19 09:50:03 +0700

    feat: dropdown folder project action click

--- components/common/DropdownFolderProject.vue
+++ components/common/DropdownFolderProject.vue
@@ -2,14 +2,25 @@
   <div>
     <button
       id="dropdownFolderProjectButton"
+      ref="dropdownFolderProjectButtonRef"
       data-dropdown-toggle="dropdown-folder-project"
       type="button"
       class="flex items-center py-1.5 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
     >
-      <svg class="mr-2" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" id="user" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v.64c.57.265.94.876.856 1.546l-.64 5.124A2.5 2.5 0 0 1 12.733 15H3.266a2.5 2.5 0 0 1-2.481-2.19l-.64-5.124A1.5 1.5 0 0 1 1 6.14V3.5zM2 6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5a.5.5 0 0 0-.5.5V6zm-.367 1a.5.5 0 0 0-.496.562l.64 5.124A1.5 1.5 0 0 0 3.266 14h9.468a1.5 1.5 0 0 0 1.489-1.314l.64-5.124A.5.5 0 0 0 14.367 7H1.633z"></path></svg>
+      <svg
+        id="user"
+        class="mr-2"
+        stroke="currentColor"
+        fill="currentColor"
+        stroke-width="0"
+        viewBox="0 0 16 16"
+        height="1em"
+        width="1em"
+        xmlns="http://www.w3.org/2000/svg"
+      ><path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v.64c.57.265.94.876.856 1.546l-.64 5.124A2.5 2.5 0 0 1 12.733 15H3.266a2.5 2.5 0 0 1-2.481-2.19l-.64-5.124A1.5 1.5 0 0 1 1 6.14V3.5zM2 6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5a.5.5 0 0 0-.5.5V6zm-.367 1a.5.5 0 0 0-.496.562l.64 5.124A1.5 1.5 0 0 0 3.266 14h9.468a1.5 1.5 0 0 0 1.489-1.314l.64-5.124A.5.5 0 0 0 14.367 7H1.633z" /></svg>
       <span class="flex items-center flex-1 mr-5">
         <span class="mr-1">Folder:</span>
-        <span>Personal Projects</span>
+        <span>{{ currentLabel }}</span>
       </span>
       <svg
         stroke="currentColor"
@@ -28,7 +39,7 @@
     <!-- Dropdown menu -->
     <div
       id="dropdown-folder-project"
-      class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+      class="z-20 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
     >
       <ul
         class="py-2 text-sm text-gray-700 dark:text-gray-200"
@@ -39,6 +50,7 @@
             v-for="(item, index) in items"
             :key="`${index}-${item.value}`"
             class="cursor-pointer hover:text-blue-400 block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            @click="selectItem(item)"
           >
             {{ item.label }}
           </div>
@@ -50,7 +62,17 @@
 
 <script lang="ts" setup>
 import { initDropdowns } from 'flowbite'
+import _find from 'lodash/find'
 import type { ILabelValue } from '~/types'
+
+const currentValue = ref<string>('personal_project')
+const dropdownFolderProjectButtonRef = ref<HTMLElement | null>(null)
+
+const currentLabel = computed(() => {
+  const item = _find(items.value, { value: currentValue.value })
+
+  return item?.label
+})
 
 const items = computed<ILabelValue[]>(() => {
   return [
@@ -61,9 +83,15 @@ const items = computed<ILabelValue[]>(() => {
     {
       value: 'personal_project',
       label: 'Personal Projects',
-    }
+    },
   ]
 })
+
+const selectItem = (item: ILabelValue) => {
+  currentValue.value = item.value
+
+  dropdownFolderProjectButtonRef.value?.click()
+}
 
 onMounted(() => {
   useFlowbite(() => {
