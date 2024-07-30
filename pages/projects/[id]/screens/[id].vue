2d6d8cd7e13a67350ef005e8d6f88f11de200635
Nguyen Quang Phu <nguyen.quang.phu@sun-asterisk.com>
2024-07-30 17:42:51 +0700

    feat: add some btn screen detail

--- pages/projects/[id]/screens/[id].vue
+++ pages/projects/[id]/screens/[id].vue
@@ -2,6 +2,24 @@
   <div>
     <HeaderPageDetail @scale="handleScale" />
 
+    <div class="flex items-center justify-between py-4 px-8">
+      <div class="ml-auto">
+        <button
+          type="button"
+          class="focus:outline-none text-white bg-green-400 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2 dark:bg-green-600 dark:hover:bg-green-400 dark:focus:ring-green-600"
+        >
+          Replace Screen
+        </button>
+      </div>
+      <div class="absolute left-1/2 transform -translate-x-1/2">
+        <button
+          type="button"
+          class="text-white bg-gray-500 hover:bg-gray-600 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2 dark:bg-gray-400 dark:hover:bg-gray-500 dark:focus:ring-gray-400 dark:border-gray-500"
+        >
+          1 of 5
+        </button>
+      </div>
+    </div>
     <div
       ref="containerRef"
       class="w-full flex items-center justify-center relative mt-10"
