8cabb566b4417718847219ec02f99936cc8be6d4
Nguyen Quang Phu <nguyen.quang.phu@sun-asterisk.com>
2024-08-02 11:41:16 +0700

    refactor: table in overview page

--- types/team.ts
+++ types/team.ts
@@ -4,3 +4,17 @@ export interface ITeamCreate {
   type: string
   emails: string
 }
+
+export interface ITeam {
+  id: number;
+  name: string;
+  color: string;
+  archived: boolean;
+  user_create_id: number;
+  privacy: string;
+  description: string;
+  website_url: string;
+  members_count: number;
+  created_at: string;
+  updated_at: string;
+}
