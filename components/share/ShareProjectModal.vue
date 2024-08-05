b44ce59777e60236e2ef58ebc44040b7163023a9
Nguyen Quang Phu <nguyen.quang.phu@sun-asterisk.com>
2024-08-05 17:10:24 +0700

    feat: update ui share project

--- components/share/ShareProjectModal.vue
+++ components/share/ShareProjectModal.vue
@@ -49,65 +49,71 @@
           name="public"
         >
           <div class="mt-2">
-            <img
-              :src="ShareIcon"
-              alt=""
-              class="my-6"
-            >
+            <p class="font-bold mb-4">
+              Manage Public Sharing Settings
+            </p>
 
-            <div class="text-sm">
-              <span>
-                Use public share link to share your screen with others. People can view or leave feedback without having an account.
+            <div class="flex items-center justify-between">
+              <span class="font-semibold">
+                Password protect
               </span>
-              <span class="text-blue-500 cursor-pointer">Update public link settings</span>
+              <el-switch />
             </div>
 
-            <div class="mt-6">
-              <div>
-                <label
-                  for="share-input"
-                  class="flex items-center justify-between mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  <span>Public Share Link</span>
-                  <!-- <span class="text-blue-500 text-xs cursor-pointer">Create New Share Link</span> -->
-                </label>
-                <div class="relative mb-6">
-                  <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                    <svg
-                      class="text-xl"
-                      stroke="currentColor"
-                      fill="currentColor"
-                      stroke-width="0"
-                      viewBox="0 0 16 16"
-                      height="1em"
-                      width="1em"
-                      xmlns="http://www.w3.org/2000/svg"
-                    ><path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" /><path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z" /></svg>
-                  </div>
-                  <input
-                    id="share-input"
-                    type="text"
-                    class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    value="hostdesignfront.vnjack.com/sharelink/1"
-                  >
+            <p class="text-xs mb-3">
+              A password will be required for anyone viewing the link
+            </p>
+
+            <form>
+              <label
+                for="set-password"
+                class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+              >Search</label>
+              <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                  <el-icon class="text-xl">
+                    <Lock />
+                  </el-icon>
                 </div>
+                <input
+                  id="set-password"
+                  type="text"
+                  class="block w-full p-4 ps-10 text-sm text-gray-900 outline-none border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                  placeholder="Enter password"
+                  required
+                >
+                <button
+                  type="submit"
+                  class="text-white absolute end-2.5 bottom-2.5 bg-green-400 hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+                >
+                  Set Password
+                </button>
               </div>
+            </form>
+
+            <div class="flex items-center mt-1 gap-1 text-xs">
+              <el-icon><InfoFilled /></el-icon>
+
+              <span>Password will be expired after 30 days.</span>
             </div>
 
-            <label class="inline-flex items-center cursor-pointer">
-              <span class="mr-2 text-sm font-medium text-gray-900 dark:text-gray-300">Disable Comments</span>
-              <input
-                type="checkbox"
-                value=""
-                class="sr-only peer"
-              >
-              <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" />
-            </label>
+            <div class="flex items-center justify-between mt-4">
+              <div>
+                <p class="mb-2 font-semibold">
+                  Disable Comments
+                </p>
+                <p class="text-xs">
+                  Anyone with this links can only view the project but can't leave any comment
+                </p>
+              </div>
+
+              <el-switch />
+            </div>
           </div>
         </el-tab-pane>
         <el-tab-pane
           label="Embed"
-          name="second"
+          name="embed"
         >
           <div
             ref="embedCodeRef"
@@ -131,11 +137,21 @@
               @click="close"
             >Cancel</span>
             <button
+              v-if="activeTab === 'embed'"
               type="button"
               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
               @click="copyToClipboard"
             >
               Copy to Clipboard
+            </button>
+
+            <button
+              v-else
+              type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              @click="saveSettings"
+            >
+              Save Settings
             </button>
           </div>
         </div>
@@ -146,7 +162,7 @@
 
 <script lang="ts" setup>
 import type { TabsPaneContext } from 'element-plus'
-import ShareIcon from '~/assets/images/icons/project/share.svg'
+import { Lock, InfoFilled } from '@element-plus/icons-vue'
 
 const emit = defineEmits(['normal-upload'])
 
@@ -164,33 +180,23 @@ const close = () => {
 }
 
 const copyToClipboard = () => {
-  if (activeTab.value === 'public') {
-    const el = document.getElementById('share-input') as HTMLInputElement
+  const el = embedCodeRef.value?.textContent as string
 
-    navigator.clipboard.writeText(el.value).then(() => {
-      ElMessage({
-        message: 'Copy successfully',
-        type: 'success',
-      })
+  navigator.clipboard.writeText(el).then(() => {
+    ElMessage({
+      message: 'Copy successfully',
+      type: 'success',
     })
-  }
-  else {
-    const el = embedCodeRef.value?.textContent as string
-
-    navigator.clipboard.writeText(el).then(() => {
-      ElMessage({
-        message: 'Copy successfully',
-        type: 'success',
-      })
-    }).catch((err) => {
-      console.error('error: ', err)
-    })
-  }
+  }).catch((err) => {
+    console.error('error: ', err)
+  })
 }
 
 const handleClick = (tab: TabsPaneContext, event: Event) => {
-  // activeTab.value = tab
+  activeTab.value = tab.paneName as string
 }
+
+const saveSettings = () => {}
 
 defineExpose({
   open,
