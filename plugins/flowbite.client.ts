ba52b5ea0d3f615ad0a572bce3ba15773882a428
Nguyen Quang Phu <quangphunguyen232@gmail.com>
2024-09-11 21:38:38 +0700

    fix: load page quick

--- plugins/flowbite.client.ts
+++ plugins/flowbite.client.ts
@@ -1,9 +1,17 @@
 import { initFlowbite } from 'flowbite';
-
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.mixin({
-    mounted () {
-      initFlowbite();
-    }
-  });
+  // nuxtApp.vueApp.mixin({
+  //   mounted () {
+  //     if (import.meta.server) {
+  //       console.log(1111)
+  //       // useFlowbite(() => {
+  //         initFlowbite();
+  //       // })
+  //     }
+  //   }
+  // });
+
+  nuxtApp.hook('page:finish', () => {
+    initFlowbite();
+  })
 });
